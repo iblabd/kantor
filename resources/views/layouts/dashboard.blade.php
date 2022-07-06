408f3aff1062a886f4530ecccf748769e6762c21
HaruSasaki <73406832+iblabd@users.noreply.github.com>
2022-07-06 13:26:25 +0700

    Update dashboard

---
+++ resources/views/layouts/dashboard.blade.php
@@ -0,0 +1,120 @@
+@extends('layouts.app')
+<style>
+    /* Profile sidebar */
+    .profile-sidebar {
+        padding: 20px 0 10px 0;
+        background: #fafcfd;
+    }
+
+    .profile-userpic img {
+        float: none;
+        margin: 0 auto;
+        width: 50%;
+        height: 50%;
+        -webkit-border-radius: 50% !important;
+        -moz-border-radius: 50% !important;
+        border-radius: 50% !important;
+    }
+
+    .profile-usertitle {
+        text-align: center;
+        margin-top: 20px;
+    }
+
+    .profile-usertitle-name {
+        color: #5a7391;
+        font-weight: 600;
+        margin-bottom: 7px;
+    }
+
+    .profile-usertitle-job {
+        text-transform: uppercase;
+        color: #5b9bd1;
+        font-weight: 600;
+        margin-bottom: 15px;
+    }
+
+    .profile-userbuttons {
+        text-align: center;
+        margin-top: 10px;
+    }
+
+    .profile-userbuttons .btn {
+        text-transform: uppercase;
+        font-weight: 600;
+        padding: 6px 15px;
+        margin-right: 5px;
+    }
+
+    .profile-userbuttons .btn:last-child {
+        margin-right: 0px;
+    }
+
+    .profile-usermenu {
+        margin-top: 30px;
+        min-width: 100%;
+    }
+
+    .profile-usermenu ul li {
+        min-width: 100%;
+        padding: 8px 12px 8px 12px;
+    }
+
+    .profile-usermenu ul li:last-child {
+        border-bottom: none;
+        min-width: 100%;
+    }
+
+    .profile-usermenu ul li a {
+        color: #93a3b5;
+        font-weight: 400;
+        text-decoration: none;
+        min-width: 100%;
+        padding: 8px 12px 8px 12px;
+    }
+
+    .profile-usermenu ul li a i {
+        margin-right: 8px;
+    }
+
+    .profile-usermenu ul li a:hover {
+        background-color: #fafcfd;
+        color: #5b9bd1;
+    }
+
+    .profile-usermenu ul li.active {
+        border-bottom: none;
+        font-weight: 400;
+        min-width: 100%;
+    }
+
+    .profile-usermenu ul li.active a {
+        color: #5b9bd1;
+        background-color: #f6f9fb;
+        border-left: 2px solid #5b9bd1;
+        width: 100%;
+    }
+
+    /* Profile Content */
+    .dashboard-content {
+        color: #5a7391;
+        padding: 20px;
+        background: #fff;
+        min-height: 100%;
+    }
+</style>
+
+
+@section('main')
+
+    <div class="container">
+        <div class="row profile">
+            @include('layouts.navbar')
+            <div class="col-md-9">
+                <div class="dashboard-content">
+                    @yield('content')
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,44 @@
+<div class="col-md-3">
+    <div class="profile-sidebar">
+        <!-- SIDEBAR USERPIC -->
+        <div class="profile-userpic">
+            {{-- <img src="" class="img-responsive" alt=""> --}}
+        </div>
+        <!-- END SIDEBAR USERPIC -->
+        <!-- SIDEBAR USER TITLE -->
+        <div class="profile-usertitle">
+            <div class="profile-usertitle-name">
+                <h5>Nama Orang</h5>
+            </div>
+            <div class="profile-usertitle-job">
+                <h6>Developer</h6>
+            </div>
+        </div>
+        <div class="profile-usermenu">
+            <ul class="nav">
+                <li class="active">
+                    <a href="#"><i class="fa-solid fa-house"></i>Overview</a>
+                </li>
+                <li>
+                    <!--masuknya ke profile setting -->
+                    <a href="#">
+                        <i class="glyphicon glyphicon-user"></i>
+                        Account Settings </a>
+                </li>
+                <li>
+                    <!--masuknya ke todo -->
+                    <a href="#" target="_blank">
+                        <i class="glyphicon glyphicon-ok"></i>
+                        Tasks </a>
+                </li>
+                <li>
+                    <!--masuknya ke absensi -->
+                    <a href="#">
+                        <i class="glyphicon glyphicon-flag"></i>
+                        Absensi </a>
+                </li>
+            </ul>
+        </div>
+        <!-- END MENU -->
+    </div>
+</div>
