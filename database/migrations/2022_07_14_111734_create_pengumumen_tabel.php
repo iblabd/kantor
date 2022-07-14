577479c7465c554c3fdab10c703ffd386a1d5796
tharissast <102042437+tharissast03@users.noreply.github.com>
2022-07-14 12:32:41 +0700

    Create 2022_07_14_111734_create_pengumumen_tabel.php

---
+++ database/migrations/2022_07_14_111734_create_pengumumen_tabel.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePengumumenTabel extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pengumumen_tabel', function (Blueprint $table) {
+            $table->id();
+            $table->String("judul");
+            $table->String("isi");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pengumumen_tabel');
+    }
+}
