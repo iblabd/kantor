fff35cc9e619f33b60bb3e33aea610ea6ff5edd7
HaruSasaki <73406832+iblabd@users.noreply.github.com>
2022-07-29 09:57:03 +0700

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,32 +2,39 @@
 
 @section('content')
 
-    @role('admin')
-        <p>Saya admin!</p>
-        <div class="table-responsive-sm">
-        <table border="2" class="table table-striped table-hover" style=" width: 200px; max-height: 200px; overflow:auto; display:inline-block" >
-            <thead>
-      <tr>
-      <th>Title</th>
-      <th>Description</th>
-
-          </tr>
-          </thead>
-          <tbody>
-          @if(count($articles) > 0)
-          @foreach($articles->all() as $article)
-          <tr>
-
-      <td>{{ $article->title }}</td>
-      <td>{{ $article->description }}</td>
-              </tr>
-              @endforeach
-             @endif
-
-            </tbody>
-          </table>
+    <div class="bg-white border rounded px-4 pt-4 pb-4">
+        <nav class="mt-4" aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item active" aria-current="page">Dasbor</li>
+            </ol>
+        </nav>
+        <div class="my-4">
+            <h2>Halo {{ auth()->user()->name }}!</h2>
         </div>
-    @else
-        <p>Saya bukan admin...</p>
-    @endrole
+
+
+        <div class="row mt-5">
+            <h4>Pengumuman</h4>
+            @if (count($articles) > 0)
+                @foreach ($articles->all() as $article)
+                    <div class="col-sm-12 my-2">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="d-inline">
+                                    <h5 class="card-title mb-0">{{ $article->title }}</h5>
+                                    <small class="card-text">{{ $article->created_at->diffForHumans() }}</small>
+                                </div>
+                                <p class="card-text mt-2">{{ $article->description }}</p>
+                                <div class="d-flex justify-content-end">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            @else
+                <div class="alert alert-warning">Tidak ada data karyawan</div>
+            @endif
+        </div>
+
+    </div>
 @endsection
